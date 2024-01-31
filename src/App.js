8dfc2b52d6126ee5ac79689283d8302011d4685a
Mariana Bojorquez <mariana_bojorquez@epam.com>
2024-01-31 11:30:22 -0600

    almost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import './App.css';
+import TodoList from './todos/TodoList';
 
-const App = () => {
+const App = () => (
     <div className="App">
-        <h1>React App!!</h1>
+        <TodoList/>
     </div>
-}
+)
 
 export default hot(module)(App);
